5d29854142baa006a99f0e5fb96b6a59a529043b
ecabrerar <eudris@gmail.com>
2018-11-30 00:41:19 -0400

    Lab04 Enumaracion y Colecciones

---
+++ introduccion-java/labs/lab04/javase_combustible_app/src/com/eudriscabrera/java/uapa/mescyt/combustible/util/Tipo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.eudriscabrera.java.uapa.mescyt.combustible.util;
+
+/**
+ *
+ * @author ecabrerar
+ */
+public enum Tipo {
+    GASOLINA,
+    GASOIL,
+    GLP,
+    GNV,
+    KEROSENE
+}
